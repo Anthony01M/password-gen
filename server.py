17d603a15490e5fbf8772fcfa905900394a3c181
Anthony M <expopulation@gmail.com>
2022-11-03 15:23:05 +0200

    will this work now

--- server.py
+++ server.py
@@ -19,19 +19,22 @@ def index():
 # Generate a random password with the given length, if it contains a number and a symbol and a capital letter and a lowercase letter then return the password (True/False) basically check if we want to include a number, symbol, capital letter, lowercase letter
 @app.route('/password', methods=['GET'])
 def password():
-    length = int(request.args.get('length'))
-    password = ""
-    for i in range(length):
-        password += random.choice(string.ascii_letters)
-    if request.args.get('number') == 'true':
-        password += random.choice(string.digits)
-    if request.args.get('symbol') == 'true':
-        password += random.choice(string.punctuation)
-    if request.args.get('capital') == 'true':
-        password += random.choice(string.ascii_uppercase)
-    if request.args.get('lowercase') == 'true':
-        password += random.choice(string.ascii_lowercase)
-    return {"password": password}
+    try:
+        length = int(request.args.get('length'))
+        password = ""
+        for i in range(length):
+            password += random.choice(string.ascii_letters)
+        if request.args.get('number') == 'true':
+            password += random.choice(string.digits)
+        if request.args.get('symbol') == 'true':
+            password += random.choice(string.punctuation)
+        if request.args.get('capital') == 'true':
+            password += random.choice(string.ascii_uppercase)
+        if request.args.get('lowercase') == 'true':
+            password += random.choice(string.ascii_lowercase)
+        return {"password": password}
+    except:
+        return {"error": "Invalid length"}
 
 # allow aws-sw.js to be loaded
 @app.route('/aws-sw.js', methods=['GET'])
@@ -39,7 +42,6 @@ def aws_sw():
     return app.send_static_file('aws-sw.js')
 
 if __name__ == '__main__':
-    
     freezer = flask_frozen.Freezer(app)
 
 
